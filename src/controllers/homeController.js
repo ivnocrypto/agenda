53329a7d5487913bdc878e901560a5c54578d71d
ivano gg <ivanogg@Mac-mini-de-ivano.local>
2022-10-22 17:01:02 -0300

    criarusuario

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,13 +1,4 @@
-exports.paginaInicial = (req, res) => {
-    res.render('index', {
-        titulo: 'Este será o título da página',
-        numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    });
-     res.render('index');
-    return;
+exports.index = (req, res) => {
+    res.render('index');
 };
 
-exports.trataPost = (req, res) => {
-    res.send(req.body);
-    return;
-};
